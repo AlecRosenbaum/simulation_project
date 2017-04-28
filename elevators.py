b73a3f7399441913df1376bb404c4a4df4162f3b
Cory Trbojevic <cmt75@pitt.edu>
2017-04-28 18:12:32 -0400

    fixed fixed sector again

--- elevators.py
+++ elevators.py
@@ -652,11 +652,14 @@ class FixedSectorsElevatorController(ElevatorController):
                 min_down_sec = min(elevator.down_sector)
                 max_down_sec = max(elevator.down_sector)
                 arr_floor = arrival[2]
+                p_dest = arrival[1]
 
                 min_up_dist = min(abs(arr_floor-min_up_sec), abs(arr_floor-max_up_sec))
                 min_down_dist = min(abs(arr_floor-min_down_sec), abs(arr_floor-max_down_sec))
 
-                if not (arr_floor > min_up_sec and arr_floor < max_up_sec) or (arr_floor > min_down_sec and arr_floor < max_down_sec):
+                if not ((arr_floor > min_up_sec and arr_floor < max_up_sec and p_dest > arr_floor)
+                        or (arr_floor > min_down_sec and arr_floor < max_down_sec
+                            and p_dest < arr_floor)):
                     fos[idx] = fos[idx]/min(min_up_dist, min_down_dist)
 
             #find the greatest figure of suitability for this arrival
