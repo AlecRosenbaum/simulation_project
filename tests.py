993479f4b53820893df43583a2eff94fc64c55d6
Alec Rosenbaum <alecr95@gmail.com>
2017-04-28 18:09:10 -0400

    oops, forgot about commented out test code. fixed.

--- tests.py
+++ tests.py
@@ -23,19 +23,15 @@ def test_scan_elevator(limit=None):
 
     # create loggers
     person_logger_path = os.path.join(dirs, settings.LOG_DIR, settings.PERSON_LOG_FNAME)
-    # person_logger = logger.PersonLogger(person_logger_path, remove_old=True)
-    # # create building
-    # building = Building([
-    #     'SB', 'B', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',])
+    person_logger = logger.PersonLogger(person_logger_path, remove_old=True)
+    # create building
+    building = Building([
+        'SB', 'B', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',])
 
-    # # generate arrivals
-    # arr_gen = ArrivalGenerator(building=building, person_logger=person_logger)
+    # generate arrivals
+    arr_gen = ArrivalGenerator(building=building, person_logger=person_logger)
 
-    # days = ["M", "Tu", "W", "Th", "F"]
-    days = []
-    arr_gen = None
-    building = None
-    person_logger = None
+    days = ["M", "Tu", "W", "Th", "F"]
 
     for day in days:
         # load saved arrivals or generate new arrivals
