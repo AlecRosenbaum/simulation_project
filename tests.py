a474a9f7a3a42d0d77694b738e29d5410fddec75
Alec Rosenbaum <alecr95@gmail.com>
2017-04-29 00:50:07 -0400

    tests for all elevators are done

--- tests.py
+++ tests.py
@@ -202,7 +202,7 @@ def test_sector_elevator(limit=None):
     # generate arrivals
     arr_gen = ArrivalGenerator(building=building, person_logger=person_logger)
 
-    days = ["M"] # , "Tu", "W", "Th", "F"]
+    days = ["M" , "Tu", "W", "Th", "F"]
 
     for day in days:
         # load saved arrivals or generate new arrivals
@@ -247,11 +247,18 @@ def test_sector_elevator(limit=None):
     sim_stats.run_stats(person_log_path=person_logger_path, stats_dir=os.path.join(dirs, "stats"))
 
 
-def test_sector_time_elevator():
-    "test for testing fixed sector algorithm"
+def test_sector_time_elevator(limit=None):
+    """test for testing fixed sector algorithm"""
+    result_dir = "FS4"
+
+    # create directory if not existing
+    dirs = os.path.join(BASE_DIR, result_dir)
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+
     # create loggers
-    person_logger = logger.PersonLogger(
-        os.path.join(settings.LOG_DIR, settings.PERSON_LOG_FNAME), remove_old=True)
+    person_logger_path = os.path.join(dirs, settings.LOG_DIR, settings.PERSON_LOG_FNAME)
+    person_logger = logger.PersonLogger(person_logger_path, remove_old=True)
     # create building
     building = Building([
         'SB', 'B', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',])
@@ -259,50 +266,49 @@ def test_sector_time_elevator():
     # generate arrivals
     arr_gen = ArrivalGenerator(building=building, person_logger=person_logger)
 
-    # load saved arrivals or generate new arrivals
-    save_path = os.path.join(settings.ARRIVALS_DIR, "test_arrivals.csv")
-    if not os.path.exists(save_path):
-        arr_gen.gen_from_classes(file_path=settings.ARRIVALS_DATA_SET_CSV)
-        arr_gen.save(save_path)
-    else:
-        arr_gen.load(save_path)
+    days = ["M", "Tu", "W", "Th", "F"]
 
-    # add first 100 floor arrival to FEQ
-    cnt = 0
-    for time, person in arr_gen.arrival_times:
-        cnt += 1
-        if cnt > 10000:
-            break
-        settings.FEQ.put_nowait((time, person, person.States.QUEUED))
+    for day in days:
+        # load saved arrivals or generate new arrivals
+        arr_gen.arrival_times = []
+        save_path = os.path.join(settings.ARRIVALS_DIR, "{}_arrivals.csv".format(day))
+        if not os.path.exists(save_path):
+            arr_gen.gen_from_classes(file_path=settings.ARRIVALS_DATA_SET_CSV, days=[day])
+            arr_gen.save(save_path)
+        else:
+            arr_gen.load(save_path)
 
-    # create 6 elevators
-    controller = elevators.FixedSectorsTimePriorityElevatorController(building)
-    controller.spawn_elevators(6, person_logger, building)
-    #SET SECTORS
-    controller.set_sector(0, ['G', '1'], ['1', '3'])
-    controller.set_sector(1, ['G', '1'], ['1', '3'])
-    controller.set_sector(2, ['G', '1'], ['1', '3'])
-    controller.set_sector(3, ['G', '1'], ['10', '12'])
-    controller.set_sector(4, ['SB', '11'], ['B', '12'])
-    controller.set_sector(5, ['SB', 'B'], ['G', '1'])
-    settings.ELEVATORS.extend(controller.elevators)
+        # add first 100 floor arrivals to FEQ
+        cnt = 0
+        for time, person in arr_gen.arrival_times:
+            cnt += 1
+            if limit is not None and cnt > limit:
+                break
+            settings.FEQ.put_nowait((time, person, person.States.QUEUED))
 
-    while not settings.FEQ.empty():
-        curr_time, obj, state = settings.FEQ.get_nowait()
-        settings.CURR_TIME = curr_time
-        obj.update_state(state)
+        # create 6 elevators
+        settings.ELEVATORS = []
+        controller = elevators.FixedSectorsTimePriorityElevatorController(building)
+        controller.spawn_elevators(6, person_logger, building)
+        #SET SECTORS
+        controller.set_sector(0, ['G', '1'], ['1', '3'])
+        controller.set_sector(1, ['G', '1'], ['1', '3'])
+        controller.set_sector(2, ['G', '1'], ['1', '3'])
+        controller.set_sector(3, ['G', '1'], ['10', '12'])
+        controller.set_sector(4, ['SB', '11'], ['B', '12'])
+        controller.set_sector(5, ['SB', 'B'], ['G', '1'])
+        settings.ELEVATORS.extend(controller.elevators)
 
-         # print system state
-        #for i in settings.ELEVATORS:
-        #    print(i)
-        #for i in building.floor_order:
-        #    print(i, building.floor[i].queue)
-        #print("------------------------------")
+        while not settings.FEQ.empty():
+            curr_time, obj, state = settings.FEQ.get_nowait()
+            settings.CURR_TIME = curr_time
+            obj.update_state(state)
 
-    # commit changes to person_logger
-    person_logger.conn.commit()
+        # commit changes to person_logger
+        person_logger.conn.commit()
+        print("Done with", day)
 
-    sim_stats.run_stats()
+    sim_stats.run_stats(person_log_path=person_logger_path, stats_dir=os.path.join(dirs, "stats"))
 
 if __name__ == '__main__':
     START = timer()
@@ -310,8 +316,8 @@ if __name__ == '__main__':
     # test_scan_elevator()
     # test_look_elevator()
     # test_nearest_elevator()
-    test_sector_elevator()
-    # test_sector_time_elevator()
+    # test_sector_elevator()
+    test_sector_time_elevator()
 
     END = timer()
     print(END - START)
